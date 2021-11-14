eab1f364835c47d35b8359e3dcf36e97839d2d56
Dario Rodriguez <dariorodriguez@MacBook-Air-de-Dario.local>
2021-11-14 13:58:24 -0500

    Hoja de vida en HTML

---
+++ README.cmd
@@ -0,0 +1,11 @@
+Hoja de Vida Personal
+De manera individual
+Crea un repositorio con los elementos básicos de una página web.
+Edita el documento HTML cambiando el titulo de la página.
+Añade elementos de tipo encabezado (h1,h2,h3, ... ) para crear las siguientes secciones para tu hoja de vida:
+Información general: Nombre y apellido, email, teléfono, ubicación actual, links a GitHub y a LinkedIn
+Experiencia, usando una lista (ul,ol) describe tu experiencia profesional, indicando el nombre de la empresa y cargo. Si aun no tienes experiencia no te preocupes pon la experiencia que te gustaría tener dentro de los siguientes 4 años.
+Habilidades técnicas y tecnologías que quieres manejar, usando una tabla (table,td,tr) describe tus habilidades técnicas indicando su nombre, e.g. Javascript, React, Firebase, etc.
+Intereses, usando una tabla (table,td,tr) describe tus intereses indicando su nombre, e.g. Aplicaciones móviles, Realidad aumentada, SCRUM, etc.
+Añade un campo de texto para un email, un textarea para un mensaje y un botón con el texto contactar
+Comparte el resultado con tus compañeros de curso en los canales públicos
